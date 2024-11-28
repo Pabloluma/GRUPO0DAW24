48cbdf9a9c6dad797553f6d96a9e18be20cfebe2
Pablo Lupión <92090787+Pabloluma@users.noreply.github.com>
2024-11-28 14:17:54 +0100

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -2,7 +2,7 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "134.0.11.190";
+     static public $mvc_bd_hostname = "localhost";
      static public $mvc_bd_nombre   = "alimentos02023";
      static public $mvc_bd_usuario  = "mymcain9391";
      static public $mvc_bd_clave    = "6O9xALme";
